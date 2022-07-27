83d61078e558c16edc4b5edec227af012c624061
sitongju <56384750+sitongju@users.noreply.github.com>
2022-07-26 21:00:59 -0700

    Delete DSCI 551 directory

--- DSCI 551/Homework 5/dataframe-c.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession\
-    .builder\
-    .appName('readfile')\
-    .getOrCreate()
-
-country = spark.read.json('country.json')
-country[['Continent']].distinct().show()
